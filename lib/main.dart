0ab70a1fb69b83aa74354bda76e2fd9a57885044
ashleehee <141728803+ashleehee@users.noreply.github.com>
2023-09-15 01:21:11 +0300

    :)

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //Color.fromARGB(255, 255, 164, 194)
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 164, 194)),
