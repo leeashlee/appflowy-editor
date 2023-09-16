6c25d6ab80a74553ca9b03fb749721ab8150aacc
ashleehee <141728803+ashleehee@users.noreply.github.com>
2023-09-16 21:14:47 +0300

    UI changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -296,26 +296,14 @@ class _HomePageState extends State<HomePage> {
         );
       } else if (currNotes.getEntry(i) is NoteCollection) {
         retVal.add(
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              alignment: Alignment.centerLeft,
-              elevation: 0.0,
-              shadowColor: Colors.transparent,
-            ),
-            onPressed: () {
-              null;
-            },
-            icon: const Icon(Icons.folder_open),
-            label: Text(currNotes.getEntry(i).getName()),
-          ),
+          _buildSeparator(context, currNotes.getEntry(i).getName()),
         );
-        /*retVal.addAll(
+        retVal.addAll(
           buildNotes(
             context,
             (currNotes.getEntry(i) as NoteCollection),
           ),
-        );*/
+        );
       }
     }
     return retVal;
