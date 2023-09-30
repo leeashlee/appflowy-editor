01a360e0bc3f1466f29aa0c11077f123fcc8b90c
leeashlee <78592522+leeashlee@users.noreply.github.com>
2023-09-30 16:47:35 +0300

    Shit likes breaking

--- lib/component/constants.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const String APPWRITE_PROJECT_ID = "65180b4ecc4e6f8eb1f9";
-const String APPWRITE_URL = "https://cloud.appwrite.io/v1";
-
-const String APPWRITE_DATABASE_ID = "6518198c77bf083a0d2c";
-const String COLLECTION_MESSAGES = "65181a10dcdcc6e17d82";
--- lib/component/editor/toolbar_items/mobile/custom_code_mobile_toolbar_item.dart
+++
@@ -1,9 +0,0 @@
-import 'package:appflowy_editor/appflowy_editor.dart';
-import 'package:flutter/material.dart';
-import 'package:noel_notes/component/icons/unicon_icons.dart';
-
-final customCodeMobileToolbarItem = MobileToolbarItem.action(
-  itemIcon: const Icon(Unicon.code),
-  actionHandler: (editorState, selection) =>
-      editorState.toggleAttribute(AppFlowyRichTextKeys.code),
-);
