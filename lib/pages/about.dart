0fcdfe3f061aeebf1a58f9747b07707c34bea7ef
leeashlee <78592522+leeashlee@users.noreply.github.com>
2023-10-02 14:35:51 +0300

    Added Privacy Policy

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:noel_notes/component/icons/unicon_icons.dart';
+import 'package:noel_notes/pages/privacy_policy.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutScreen extends StatelessWidget {
@@ -51,6 +52,18 @@ class AboutScreen extends StatelessWidget {
                 if (!await launchUrl(url)) {
                   throw Exception('Could not launch $url');
                 }
+              },
+            ),
+            MenuItemButton(
+              leadingIcon: const Icon(Unicon.coffee),
+              child: const Text("Privacy Policy"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const PrivacyPolicy(),
+                  ),
+                );
               },
             ),
             MenuItemButton(
