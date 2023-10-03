b8822e06fca11ffe2b10dc936cec001f925925dc
leeashlee <78592522+leeashlee@users.noreply.github.com>
2023-10-03 21:13:02 +0300

    Update login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,10 +10,10 @@ class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  LoginPageState createState() => LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class LoginPageState extends State<LoginPage> {
   final emailTextController = TextEditingController();
   final passwordTextController = TextEditingController();
   bool loading = false;
